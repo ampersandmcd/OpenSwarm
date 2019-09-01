9155dd227dcc8f369b52d19f2722fccf11d81316
andrewmcdonald27 <andrewmcdonald27@gmail.com>
2019-09-01 13:43:53 -0400

    refactor config into Configuration.h

--- Arduino/src/Pathfinder.ino
+++ Arduino/src/Pathfinder.ino
@@ -2,41 +2,17 @@
  * Andrew McDonald
  * MSU D-CYPHER Lab
  * Pathfinder.ino
- * 
  */
 
+// include external libraries
 #include "WiFiEsp.h"
 #include "WiFiEspUdp.h"
 #include "SoftwareSerial.h"
 #include "MMEmotor.h"
 
-struct Configuration 
-{
-  // identifier configuration
-  int ID = 1;
+// include user-defined code
+#include "include/Configuration.h"
 
-  // network configuration
-  char SSID[64] = "belkin.785";
-  char Password[64] = "cyb2e9ce";
-  int RXPort = 8080;
-  int TXPort = 8000 + ID;
-  unsigned long SerialBaud = 115200;
-  int ESPBaud = 9600;
-
-  // preferences
-  bool Debug = true;
-
-  // pin configuration
-  int MotorPWMA = 5;
-  int MotorAIN1 = 7;
-  int MotorAIN2 = 6;
-  int MotorSTBY = 8;
-  int MotorPWMB = 11;
-  int MotorBIN1 = 9;
-  int MotorBIN2 = 10;
-  int SoftwareSerialRX = 2;
-  int SoftwareSerialTX = 3;
-};
 
 // create configuration object
 Configuration Config = *(new Configuration());
---
+++ Arduino/src/include/Configuration.h
@@ -0,0 +1,27 @@
+struct Configuration 
+{
+  // identifier configuration
+  int ID = 1;
+
+  // network configuration
+  char SSID[64] = "belkin.785";
+  char Password[64] = "cyb2e9ce";
+  int RXPort = 8080;
+  int TXPort = 8000 + ID;
+  unsigned long SerialBaud = 115200;
+  int ESPBaud = 9600;
+
+  // preferences
+  bool Debug = true;
+
+  // pin configuration
+  int MotorPWMA = 5;
+  int MotorAIN1 = 7;
+  int MotorAIN2 = 6;
+  int MotorSTBY = 8;
+  int MotorPWMB = 11;
+  int MotorBIN1 = 9;
+  int MotorBIN2 = 10;
+  int SoftwareSerialRX = 2;
+  int SoftwareSerialTX = 3;
+};
